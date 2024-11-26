cdf8e5fc402f59de698d4babc7f5f53af032e58b
Tiago Marques <tiagomarques@Tiagos-MBP.lan>
2024-11-25 18:28:03 -1000

    adding entitites like sonic, ring and motobug

---
+++ entities/sonic.js
@@ -0,0 +1,38 @@
+import k from '../src/kaplayCtx';
+
+export function makeSonic(pos){
+    const sonic = k.add([
+        k.sprite("sonic", {anim: "run"}),
+        k.scale(4),
+        k.area(),
+        k.anchor("center"),
+        k.pos(pos),
+        k.body({jumpForce: 1700 }),
+        {
+            ringCollectUI: null,
+            setControls(){
+                k.onButtonPress("jump", () => {
+                    if (this.isGrounded()){
+                        this.play("jump");
+                        this.jump();
+                        k.play("jump", { volume: 0.5 }); //this method plays the sound - it is a method from the global (object)
+                    }
+                });
+            },
+            setEvents(){
+                this.onGround(() => {
+                    this.play("run");
+                })
+            }
+        }
+    ]);
+
+    sonic.ringCollectUI = sonic.add([
+        k.text("", { font: "mania", size: 24 }),
+        k.color(255, 255, 0),
+        k.anchor("center"),
+        k.pos(30, -10),
+    ])
+
+    return sonic;
+}
